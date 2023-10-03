5790b762d395fc62e0ea8f178c8d6354410a8174
Roan Boerlage <roanboerlage@gmail.com>
2023-10-03 10:52:10 +0200

    Adding getScore() method

--- Memory.Model/Game.cs
+++ Memory.Model/Game.cs
@@ -2,6 +2,27 @@
 {
     public class Game
     {
+        public DateTime StartTime { get; set; } = DateTime.Now;
 
+        public DateTime EndTime { get; set; } 
+
+        public List<Card> Cards = new List<Card> { };
+
+        public int Attempts {  get; set; }
+
+        public int GetScore()
+        {
+            //Besteedde tijd berekenen
+            TimeSpan timespent = EndTime - StartTime;
+            int time = Math.Abs(timespent.Seconds);
+
+            //Kwadraat van kaart uitrekenen
+            int squareofcards = (int)Math.Pow(Cards.Count(), 2);
+            
+            //Score berekenen
+            int score = (squareofcards / (time * Attempts)) * 1000;
+
+            return score;
+        }
     }
 }
