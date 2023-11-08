3e6718295e739745725b27d54d98f08190ac0b2f
Roan Boerlage <roanboerlage@gmail.com>
2023-11-08 12:31:02 +0100

    Simpeler, werkend

--- Memory.Model/Classes/Game.cs
+++ Memory.Model/Classes/Game.cs
@@ -82,84 +82,88 @@ namespace Memory.Model.Classes
         }
 
         //Functie om een kaart te kiezen.
+        //Deze functie moet eigenlijk alleen voor de console versie gebruikt worden.
+        //Bij GUI Direct naar comparecards gaan.
         //TODO: Refactoren, simpeler en testbaarder maken.
         public void ChooseCards()
         {
-            Console.WriteLine("Pick a first card.");
-            Card choice1 = CheckIfCardExists(Console.ReadLine());
+            bool validInput = false;
 
-            Console.WriteLine("Pick a second card.");
-            Card choice2 = CheckIfCardExists(Console.ReadLine());
-
-            if (choice1.CardValue == choice2.CardValue && 
-                choice1.Id != choice2.Id && 
-                !choice1.TurnedOver && 
-                !choice2.TurnedOver)
+            while (!validInput)
             {
-                choice1.TurnedOver = true;
-                choice2.TurnedOver = true;
-                Console.WriteLine("Correct!");
-            } 
-            else if (choice1.Id == choice2.Id)
-            {
-                Console.WriteLine("You cannot pick the same card twice, try again. An attempt has not been added.");
-                ChooseCards();           
-            } else if (choice1.TurnedOver || choice2.TurnedOver)
-            {
-                Console.WriteLine("One of the cards was already turned over, try again. An attempt has not been added.");
-                ChooseCards();
-            }
-            else
-            {
-                Console.WriteLine("Those are not the same cards! Try again. An attempt has been added.");
-            }
-            Attempts++;
-        }
-
-        //Meer linq gebruiken.
-        //Kan misschien nog opgedeeld worden?
-        public Card CheckIfCardExists(string choice)
-        {
-            Card pickedCard = null;
-            bool validChoice = false;
-
-            while (!validChoice)
-            {
+                Console.WriteLine("Pick the first card.");
+                Card choice1 = null;
                 try
                 {
-                    int parsedChoice = int.Parse(choice);
-                    //Kan out of bounds error geven.
-                    try
-                    {
-                        pickedCard = Cards[parsedChoice - 1];
-                    } catch (ArgumentOutOfRangeException ex)
-                    {
-                        pickedCard = null;
-                    }
-                    
-                    if (pickedCard == null)
-                    {
-                        throw new CardNotFoundException($"The card with id {choice} does not exist.");
-                    } else
-                    {
-                        validChoice = true;
-                    }
-                } catch (FormatException)
+                    choice1 = Check(Console.ReadLine());
+                }
+                catch (CardNotFoundException ex)
                 {
-                    Console.WriteLine($"{choice} is not a valid number.");
-                } catch (CardNotFoundException ex)
-                {
-                    Console.WriteLine(ex.Message);
+                    Console.WriteLine($"Error: {ex.Message}");
+                    continue; 
                 }
 
-                if (!validChoice)
+                Console.WriteLine("Pick the second card.");
+                Card choice2 = null;
+                try
                 {
-                    Console.Write("Enter a valid choice: ");
-                    choice = Console.ReadLine();
+                    choice2 = Check(Console.ReadLine());
+                }
+                catch (CardNotFoundException ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    continue;
+                }
+
+                if (choice1 != null && choice2 != null)
+                {
+                    if (CompareCards(choice1, choice2))
+                    {
+                        validInput = true;
+                    }
                 }
             }
+        }
 
-            return pickedCard;
+
+        public Card Check(string input)
+        {
+            try
+            {
+                int parsed = ParseStringToInt(input);
+                Card card = GetCardFromCards(parsed);
+                return card;
+            } catch (CardNotFoundException e) 
+            {
+                throw new CardNotFoundException($"{e}");
+            }
+            
+        }
+
+        public int ParseStringToInt(string numberAsString)
+        {
+            try
+            {
+                int number = int.Parse(numberAsString);
+                return number;
+            }
+            catch
+            {
+                throw new CardNotFoundException("Number is not valid.");
+            }
+        }
+
+        public Card GetCardFromCards(int number)
+        {
+            try
+            {
+                Card card = Cards[number - 1];
+                return card;
+            }
+            catch (Exception)
+            {
+                throw new CardNotFoundException("The card could not be found.");
+            }
         }
 
         //Checked of alle kaarten omgedraaid zijn.
@@ -179,16 +183,32 @@ namespace Memory.Model.Classes
             }
         }
 
-        public int Parser(string numberAsString)
+        public bool CompareCards(Card choice1, Card choice2)
         {
-            try
+            if (choice1.CardValue == choice2.CardValue &&
+               choice1.Id != choice2.Id &&
+               !choice1.TurnedOver &&
+               !choice2.TurnedOver)
             {
-                int number = int.Parse(numberAsString);
-                return number;
-            } catch
+                choice1.TurnedOver = true;
+                choice2.TurnedOver = true;
+                Console.WriteLine("Correct!");
+                return true;
+            }
+            else if (choice1.Id == choice2.Id)
             {
-                Console.WriteLine("Incorrect number, type a whole number.");
-      
+                Console.WriteLine("You cannot pick the same card twice, try again. An attempt has not been added.");
+                return false;
+            }
+            else if (choice1.TurnedOver || choice2.TurnedOver)
+            {
+                Console.WriteLine("One of the cards was already turned over, try again. An attempt has not been added.");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Those are not the same cards! Try again. An attempt has been added.");
+                return true;
             }
         }
     }
