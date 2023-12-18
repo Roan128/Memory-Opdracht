ecebb53ad9e29a2826a204d3162811febf7c1cc2
Roan Boerlage <roanboerlage@gmail.com>
2023-12-18 16:59:39 +0100

    Refactoring

--- Memory.Test/GlobalUsings.cs
+++ Memory.Test/GlobalUsings.cs
@@ -1,4 +1,4 @@
+global using Memory.BLL.BusinessObjects;
 global using Memory.DAL.Services;
-global using Memory.Model.BusinessObjects;
 global using Memory.Model.Exceptions;
 global using NUnit.Framework;
